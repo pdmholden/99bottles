09d0cfa890fbeaa5cc203e8c394c9173f08f73ac
Paul Holden <paul.holden@clio.com>
2017-09-04 14:54:37 -0700

    Separate into first/last sentence

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,53 +9,38 @@ class Bottles
   end
 
   def verse(number)
-    line_one(number) + line_two(number) + line_three(number) + line_four(number)
+    sentence_one(number) + sentence_three(number)
   end
 
   private
 
-  def line_one(number)
+  def sentence_one(number)
     case number
     when 0
-      "No more bottles of beer on the wall, "
-    when 1
-      "1 bottle of beer on the wall, "
-    else
-      "#{number} bottles of beer on the wall, "
-    end
-  end
-
-  def line_two(number)
-    case number
-    when 0
+      "No more bottles of beer on the wall, " +
       "no more bottles of beer.\n"
     when 1
+      "1 bottle of beer on the wall, " +
       "1 bottle of beer.\n"
     else
+      "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n"
     end
   end
 
-  def line_three(number)
+  def sentence_three(number)
     case number
     when 0
-      "Go to the store and buy some more, "
-    when 1
-      "Take it down and pass it around, "
-    else
-      "Take one down and pass it around, "
-    end
-  end
-
-  def line_four(number)
-    case number
-    when 0
+      "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     when 1
+      "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     when 2
+      "Take one down and pass it around, " +
       "1 bottle of beer on the wall.\n"
     else
+      "Take one down and pass it around, " +
       "#{number-1} bottles of beer on the wall.\n"
     end
   end
