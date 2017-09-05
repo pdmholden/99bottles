966e343bbe53141f39ed33a7f857f35fcb25b5f7
Paul Holden <paul.holden@clio.com>
2017-09-04 21:58:08 -0700

    Extract action line to remove another difference

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -12,12 +12,12 @@ class Bottles
     when 0
       "No more #{container(number)} of beer on the wall, " +
       "no more #{container(number)} of beer.\n" +
-      "Go to the store and buy some more, " +
+      take_appropriate_action(number) +
       "99 #{container(99)} of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
-      "Take #{single(number)} down and pass it around, " +
+      take_appropriate_action(number) +
       "#{number_left(number)} #{container(number-1)} of beer on the wall.\n"
     end
   end
@@ -43,6 +43,14 @@ class Bottles
       "no more"
     else
       starting_number - 1
+    end
+  end
+
+  def take_appropriate_action(number)
+    if (number == 0)
+      "Go to the store and buy some more, "
+    else
+      "Take #{single(number)} down and pass it around, "
     end
   end
 end
