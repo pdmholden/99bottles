2ad90ca0a9f20476ad15463820b76c8823408acd
Paul Holden <paul.holden@clio.com>
2017-09-23 12:09:20 -0700

    Answering questions

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,24 @@
+# characteristics:
+# - small methods
+# - always passing around a number
+# - need to look at API to understand private methods (ie, they don't
+#   explain themselves)
+#
+# thoughts:
+# - like the breakdown of public API
+# - like the simplicity of small methods
+#
+# questions:
+# - wtf is shape? (special_condition) ? blah : halb
+# - all use argument of same name, which is the same thing
+# - args of the same name, mean the same thing, but not always obvious what
+#   the param's relevance is to the method's purpose
+# - private below #verse
+# - classes? verse/song?
+# - base/general case + special case
+# - two branches for conditionals in small methods
+# - never any code other than conditionals
+
 class Bottles
   def song
     verses(99, 0)
@@ -14,6 +35,8 @@ class Bottles
     "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
   end
 
+  # explains what's special, ie, 0
+  # 9 - depends more on the param
   def quantity(number)
     if number == 0
       "no more"
@@ -22,6 +45,8 @@ class Bottles
     end
   end
 
+  # explains what's special, ie, 1
+  # 9 - depends more on the param
   def container(number)
     if number == 1
       "bottle"
@@ -30,6 +55,8 @@ class Bottles
     end
   end
 
+  # parameter has nothing to do with meaning of method
+  # 9 - depends more on the class? what to do with bottles/beer/containers?
   def action(number)
     if number == 0
       "Go to the store and buy some more"
@@ -38,6 +65,9 @@ class Bottles
     end
   end
 
+  # explains what's special, ie, 1
+  # related to container? or is that the abstraction we avoided?
+  # 9 - depends more on the param
   def pronoun(number)
     if number == 1
       "it"
@@ -46,6 +76,11 @@ class Bottles
     end
   end
 
+  # maybe could have a better name
+  # explains what's special, ie, 0
+  # not clear why go back to 99
+  # 9 - depends more on the class? param is important but related logic seems
+  #     to be something else, maybe a class
   def successor(number)
     if number == 0
       99
