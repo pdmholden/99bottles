40e332f92b6d96be2f707fa40afec8a56ecff114
Paul Holden <paul.holden@clio.com>
2017-09-26 20:26:38 -0700

    BottleFactory that just returns BottleNumber

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,13 +8,19 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number      = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number      = BottleFactory.newBottleNumber(number)
+    next_bottle_number = BottleFactory.newBottleNumber(bottle_number.successor)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
     "#{next_bottle_number} of beer on the wall.\n"
+  end
+end
+
+class BottleFactory
+  def self.newBottleNumber(number)
+    BottleNumber.new(number)
   end
 end
 
